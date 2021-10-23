a9bc6ad1b329acfcf78436316e5a842e8c3a79b6
Andres Serna <sernmandres@gmail.com>
2021-10-23 09:21:23 -0500

    Entrega 3, detalles de peliculas

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { MoviesService } from 'src/app/services/movies.service';
-import { PeliculaDetalle } from '../../interfaces/interfaces';
+import { PeliculaDetalle, Cast } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-detalle',
@@ -9,19 +10,31 @@ import { PeliculaDetalle } from '../../interfaces/interfaces';
 })
 export class DetalleComponent implements OnInit {
   @Input() id;
-  pelicula: PeliculaDetalle;
+  pelicula: PeliculaDetalle = {};
+  actores: Cast[] = [];
+  oculto = 150;
 
-  constructor( private moviesService: MoviesService) { }
+  slideOptActores = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: -5,
+  };
+
+  constructor( private moviesService: MoviesService, private modalCtrl: ModalController) { }
 
   ngOnInit() {
     this.moviesService.getPeliculaDetalle(this.id).subscribe(resp => {
-      console.log(resp);
+      console.log("id::>" , this.id);
       this.pelicula = resp;
     });
 
     this.moviesService.getActoresPelicula(this.id).subscribe(resp => {
-      console.log(resp);
+      this.actores = resp.cast;
     });
+  }
+
+  regresar() {
+    this.modalCtrl.dismiss();
   }
 
 }
